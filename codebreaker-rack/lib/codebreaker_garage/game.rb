b813965ffce1f6394f40d4072e3f51c32cb7ac03
StanisLoveSid <joingamings@gmail.com>
2016-12-19 16:02:02 +0200

    main addition

---
+++ codebreaker-rack/lib/codebreaker_garage/game.rb
@@ -0,0 +1,67 @@
+module Codebreaker_garage
+  class Game
+    attr_accessor :attempts, :win, :generated_code, :hints
+
+    HINTS = 3
+    ATTEMPTS = 9
+
+    def initialize
+      @generated_code = []
+      @attempts = ATTEMPTS
+      @hints = HINTS
+      @win = false
+    end
+
+    def start
+      @generated_code = (1..4).map { rand(1..6) }
+    end
+
+    def guesser(guessed_code)
+      @attempts -= 1
+      converted = guessed_code.split('').map(&:to_i)
+      result = ""
+      if @generated_code == converted
+        @win = true
+        return "++++"
+      end
+      associative_array = @generated_code.zip(converted)
+      associative_array.each do |array|
+        next unless array[0] == array[1]
+        result << "+"
+        array[0] = array[1] = nil
+      end
+      not_exact_match(associative_array, result)
+    end
+
+    def not_exact_match(associative_array, result)
+      associative_array.reject! {|element| element == [nil, nil]}
+      transposed = associative_array.transpose
+      transposed[0].each do |digit|
+        next unless transposed[1].include?(digit)
+        result << '-'
+        transposed[1][transposed[1].find_index(digit)] = nil
+      end
+      result
+    end
+
+    def hint
+      @hints -= 1
+      machine_codebreaker = (1..6).to_a
+      hint = machine_codebreaker.repeated_permutation(4).to_a
+      hint.select! {|code| code == @generated_code }
+      hint.flatten![rand(4)]
+    end
+
+    def statistics(username)
+      {
+        name: username,
+        victory: @win,
+        user_attempts_left: ATTEMPTS - @attempts,
+        user_hints_left: HINTS - @hints,
+        time: Time.now,
+        secret_code: @generated_code.to_s
+      }
+    end
+
+  end
+end
---
+++ codebreaker-rack/lib/codebreaker_garage/racker.rb
@@ -0,0 +1,106 @@
+$LOAD_PATH << File.dirname(__FILE__)
+require 'erb'
+require 'game'
+require 'yaml'
+require 'psych'
+
+class Racker
+  include Codebreaker_garage
+
+  def self.call(env)
+    new(env).response.finish
+  end
+
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
+
+  def response
+    case @request.path
+    when "/" then Rack::Response.new(render("index.html.erb"))
+    when "/start" then game_start
+    when "/make_guess"  then make_guess
+    when "/save_score" then save_score
+    when "/game_over" then game_over
+    when "/score_table" then Rack::Response.new(render("score_table.html.erb"))
+    else
+      Rack::Response.new("Not Found", 404)
+    end
+  end
+
+  def game_start
+    @request.session.clear
+    @request.session[:game] = Game.new
+    @request.session[:game].start
+    redirect_to('/')
+  end
+
+  def game_over
+    Rack::Response.new(render("game_over.html.erb"))
+  end
+
+  def make_guess
+    if game.attempts > 0
+      if (@request.params['guess'] == 'hint') && (game.hints > 0)
+        @request.session[:hint] = game.hint
+      else
+        @request.session[:guess] = @request.params['guess']
+        @request.session[:mark] = game.guesser(guess)
+      end
+      game.win ? redirect_to('/game_over') : redirect_to('/')
+    else
+      redirect_to('/game_over')
+    end
+  end
+
+  def game
+    @request.session[:game]
+  end
+
+  def guess
+    @request.session[:guess]
+  end
+
+  def mark
+    @request.session[:mark]
+  end
+
+  def hint
+    @request.session[:hint]
+  end
+
+  def hints_left
+    Game::HINTS - game.hints
+  end
+
+  def attempts_left
+    Game::ATTEMPTS - game.attempts
+  end
+
+  def save_score
+    username = @request.params['username']
+    @request.session[:username] = username
+    save(game.statistics(username))
+    redirect_to('/start')
+  end
+
+  def render(template)
+    path = File.expand_path("../../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+
+  private
+
+  def save(data)
+    File.open("score.yml", "a") { |file| file << data.to_yaml }
+  end
+
+  def load(filename)
+    Psych.load_stream(File.read(filename))
+  end
+
+  def redirect_to(path)
+    Rack::Response.new { |response| response.redirect(path) }
+  end
+
+end
